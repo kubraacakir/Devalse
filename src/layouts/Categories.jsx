c1c500cc842863b931eab2b7b68cf58d9eb3954e
serkan.ceviz <serkan.ceviz@fonetbt.com>
2023-10-01 15:44:33 +0300

    Added some responsive backend codes

--- src/layouts/Categories.jsx
+++ src/layouts/Categories.jsx
@@ -1,89 +1,88 @@
-import React, { Component} from 'react'
-import { Link } from 'react-router-dom'
-import { Menu } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
 
 export default class Categories extends Component {
-
-  state = { activeItem: '' }
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  state = { activeItem: "" };
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
   render() {
-    const { activeItem } = this.state
+    const { activeItem } = this.state;
     return (
       <div>
         <Menu pointing vertical>
           <Menu.Item
-            name='Metin'
-            active={activeItem === 'Metin'}
+            name="Metin"
+            active={activeItem === "Metin"}
             onClick={this.handleItemClick}
             as={Link}
             to="/"
           />
           <Menu.Item
-            name='Karakter'
-            active={activeItem === 'Karakter'}
+            name="Karakter"
+            active={activeItem === "Karakter"}
             onClick={this.handleItemClick}
             as={Link}
             to="/"
           />
           <Menu.Item
-            name='Restoran'
-            active={activeItem === 'Restoran'}
+            name="Restoran"
+            active={activeItem === "Restoran"}
             onClick={this.handleItemClick}
             as={Link}
             to="/"
           />
           <Menu.Item
-            name='Kafe'
-            active={activeItem === 'Kafe'}
+            name="Kafe"
+            active={activeItem === "Kafe"}
             onClick={this.handleItemClick}
             as={Link}
             to="/"
           />
           <Menu.Item
-            name='Müzik'
-            active={activeItem === 'Müzik'}
+            name="Müzik"
+            active={activeItem === "Müzik"}
             onClick={this.handleItemClick}
             as={Link}
             to="/"
           />
           <Menu.Item
-            name='Alışveriş'
-            active={activeItem === 'Alışveriş'}
+            name="Alışveriş"
+            active={activeItem === "Alışveriş"}
             onClick={this.handleItemClick}
             as={Link}
             to="/"
           />
           <Menu.Item
-            name='Proje'
-            active={activeItem === 'Proje'}
+            name="Proje"
+            active={activeItem === "Proje"}
             onClick={this.handleItemClick}
             as={Link}
             to="/"
           />
           <Menu.Item
-            name='Yönlendirme'
-            active={activeItem === 'Yönlendirme'}
+            name="Yönlendirme"
+            active={activeItem === "Yönlendirme"}
             onClick={this.handleItemClick}
             as={Link}
             to="/"
           />
           <Menu.Item
-            name='Tabela'
-            active={activeItem === 'Tabela'}
+            name="Tabela"
+            active={activeItem === "Tabela"}
             onClick={this.handleItemClick}
             as={Link}
             to="/"
           />
           <Menu.Item
-            name='Ürünlerimiz'
-            active={activeItem === 'Ürünlerimiz'}
+            name="Ürünlerimiz"
+            active={activeItem === "Ürünlerimiz"}
             onClick={this.handleItemClick}
             as={Link}
             to="/urunler"
           />
         </Menu>
       </div>
-    )
+    );
   }
 }
--- src/pages/UrunEkle.jsx
+++ src/pages/UrunEkle.jsx
@@ -1,23 +1,26 @@
-import { Form, Formik } from 'formik'
-import React, { useState, useEffect } from 'react'
-import * as Yup from "yup"
-import MyTextInput from '../utilities/MyTextInput'
-import { Button, Dropdown } from 'semantic-ui-react'
-import IsletmeService from '../services/IsletmeService'
-import { NavLink } from 'react-router-dom'
-
+import { Form, Formik } from "formik";
+import React, { useState, useEffect } from "react";
+import * as Yup from "yup";
+import MyTextInput from "../utilities/MyTextInput";
+import { Button, Dropdown } from "semantic-ui-react";
+import { IsletmeService } from "../services/IsletmeService";
+import { NavLink } from "react-router-dom";
 
 export default function UrunEkle() {
-
-  const initialValues = { productName: "", price: "", categoryId: "", productImage: "" }
+  const initialValues = {
+    productName: "",
+    price: "",
+    categoryId: "",
+    productImage: "",
+  };
   const schema = Yup.object({
     categoryName: Yup.string().required("Kategori adı zorunludur."),
     productImage: Yup.string().required("Ürün fotoğrafının urli zorunludur."),
     productName: Yup.string().required("Ürün adı zorunludur."),
-    price: Yup.number()
-  })
+    price: Yup.number(),
+  });
 
-  const { state, setState } = useState([])
+  const { state, setState } = useState([]);
 
   // useEffect(() => {
   //   let isletmeService = new IsletmeService
@@ -29,29 +32,34 @@ export default function UrunEkle() {
       initialValues={initialValues}
       validationSchema={schema}
       onSubmit={(values) => {
-        console.log(values)
-        let isletmeService = new IsletmeService()
-        isletmeService.saveProduct(values.productName, values.price, values.categoryName, values.productImage)
+        console.log(values);
+        let isletmeService = new IsletmeService();
+        isletmeService.saveProduct(
+          values.productName,
+          values.price,
+          values.categoryName,
+          values.productImage
+        );
       }}
     >
-      <Form className='ui form'>
+      <Form className="ui form">
         <MyTextInput name="productName" placeholder="Ürün Adı" />
         <MyTextInput name="categoryId" placeholder="Kategori Adı" />
 
         <Dropdown
-          placeholder='Kategori'
+          placeholder="Kategori"
           fluid
           selection
           options={state}
-          onChange={(value) => {
-              
-          }}
+          onChange={(value) => {}}
         />
 
         <MyTextInput name="productImage" placeholder="Ürün Fotoğraf Urli" />
         <MyTextInput name="price" placeholder="Ürün Fiyatı" />
-        <Button color='green' type='submit'>Ekle</Button>
+        <Button color="green" type="submit">
+          Ekle
+        </Button>
       </Form>
     </Formik>
-  )
+  );
 }
